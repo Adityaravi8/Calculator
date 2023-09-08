c6eaa7cbf73429b4ab3b0e3ff5c099f78df76263
Aditya Ravi <94879854+Adityaravi8@users.noreply.github.com>
2023-09-08 17:35:20 -0400

    Finished Calculator
    Finished the javascript needed to make this calculator function.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Calculator
+
+A simple interactive calculator using HTML, Javascript and CSS
+
+By Aditya Ravi
---
+++ function.js
@@ -0,0 +1,25 @@
+function addtoDisplay(value) {
+
+    document.getElementById('display').value += value;
+}
+
+function allClear() {
+
+    document.getElementById('display').value = '';
+}
+
+function deleteButton() {
+
+    let output = document.getElementById('display');
+    output.value = output.value.splice(0, -1);
+}
+
+function calculate() {
+    let output = document.getElementById('display');
+
+    try {
+        output.value = eval(output.value);
+    } catch (error) {
+        output.value = 'Error';
+    }
+}
